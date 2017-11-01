1829df153cd8b152a5a0cfb64ce5147c01e0ae77
yeosu0107 <yeosu0107@naver.com>
2017-11-01 14:17:21 +0900

    winsock 설정

--- NetWorkTermProject/MarioEXODUS_Server/MarioEXODUS_Server/main.cpp
+++ NetWorkTermProject/MarioEXODUS_Server/MarioEXODUS_Server/main.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#pragma once
+#include "stdafx.h"
 
 int main(void)
 {
+	int retval;
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// socket()
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_display("socket()");
+
+	// bind()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_display("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_display("listen()");
+	
+	// closesocket()
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
 	return 0;
 }
---
+++ NetWorkTermProject/MarioEXODUS_Server/MarioEXODUS_Server/stdafx.h
@@ -0,0 +1,22 @@
+#pragma once
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+#define SERVERPORT 9000
+
+// 소켓 함수 오류 출력
+void err_display(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char *)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
